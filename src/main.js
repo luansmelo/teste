b65e6836dd286fabf5583012f2ed1cb6b7e01976
luansmelo <luan.santos@labenu.com.br>
2023-04-19 14:21:18 -0300

    teste oito

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 const gameLogic = require("./gameLogic");
 
-let getUserInput;
-
-if (typeof window !== "undefined") {
-  getUserInput = (message) => {
-    return prompt(message);
-  };
-} else {
-  const readlineSync = require("readline-sync");
-  getUserInput = (message) => {
-    return readlineSync.question(message);
-  };
-}
+let getUserInput = (message) => {
+  return prompt(message);
+};
 
 gameLogic.startGame(getUserInput);
